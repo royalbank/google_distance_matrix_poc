cc40b229a3e3962c84489fb9ed05e1a0615da7b6
royalbank <luyao@live.ca>
2017-09-04 22:38:33 -0400

    added proxy

--- initial/src/main/java/geo/AppConfiguration.java
+++ initial/src/main/java/geo/AppConfiguration.java
@@ -9,7 +9,10 @@ import com.google.maps.GeoApiContext;
 public class AppConfiguration {
 	@Bean
 	public GeoApiContext myService() { 
-
+	    System.setProperty("http.proxyHost", "oproxy.fg.rbc.com");
+	    System.setProperty("http.proxyPort", "8080");
+	    System.setProperty("https.proxyHost", "oproxy.fg.rbc.com");
+        System.setProperty("https.proxyPort", "8080");
 		GeoApiContext context = new GeoApiContext().setApiKey(GeoConst.API_KEY);
 		return context;
 	}
