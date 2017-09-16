f9cf78b7c3238f7fc5c6b939b25e54db35a903dc
Luyao Zhao <luyao@live.ca>
2017-09-16 12:01:00 -0400

    2222

--- src/main/java/geo/GeoController.java
+++ src/main/java/geo/GeoController.java
@@ -20,26 +20,26 @@ public class GeoController{
 	@Autowired
 	GeoService geoService;
 
-	@RequestMapping(value = "/branches?search=distances", method = RequestMethod.POST)
+	@RequestMapping(value = "/distances", method = RequestMethod.POST)
 	public @ResponseBody DistanceMatrix getDistances(@RequestBody DistanceDTO distanceDTO) {
-
+	
 		return geoService.getDistances(distanceDTO.getSrcCoord().toModel(),
 				LatLngDTO.toModels(distanceDTO.getDestCoords()));
 	}
 
-	@RequestMapping(value = "/branches?search=nearby", method = RequestMethod.POST)
+	@RequestMapping(value = "/nearby", method = RequestMethod.POST)
 	public @ResponseBody List<BranchDTO> getNearbyBranches(@RequestParam double srcLat, @RequestParam double srcLng,
 			@RequestBody List<BranchDTO> branches) {
-
+	
 		return geoService.getDistances(new LatLng(srcLat, srcLng), branches);
 	}
 
-	@RequestMapping(value = "/branches?search=coordinates", method = RequestMethod.GET)
+	@RequestMapping(value = "/coordinates", method = RequestMethod.GET)
 	public @ResponseBody LatLng getDistances(@RequestParam String address) {
 		return geoService.getLatLng(address);
 	}
 
-	@RequestMapping(value = "/branches?search=enrich", method = RequestMethod.POST)
+	@RequestMapping(value = "/enrich", method = RequestMethod.POST)
 	public @ResponseBody List<BranchDTO> getDistances(@RequestBody List<BranchDTO> branches) {
 		return geoService.enrichAddress(branches);
 	}
